211cbba37557febb0067340d2d399a4c652cf19d
pasan <pasanudara356@gmail.com>
2025-08-27 20:12:51 +0530

    initial setup

--- src/socket/bid.socket.js
+++
@@ -1,8 +0,0 @@
-export const bidSocketHandler = (io, socket) => {
-    //Listen for bid placement
-    socket.on('placeBid', (data) => {
-        console.log("Bid received:", data);
-        //Broadcast the new bid to all connected clients
-        io.emit('newBid', data);
-    });
-};
